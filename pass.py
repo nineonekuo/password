5cd221b6735064ef392165e96a660f34123e8794
nineonekuo <guo3325303@gmail.com>
2018-10-31 15:54:47 +0800

    change i>0

--- pass.py
+++ pass.py
@@ -1,6 +1,6 @@
 password = 'abc'
 a = 3 #剩餘機會
-while True:
+while a > 0:
 	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print('登入成功')
@@ -8,6 +8,3 @@ while True:
 	else:
 		a = a - 1
 		print('密碼錯誤! 還有',a,'次機會')
-		if a == 0:
-			print('登入失敗')
-			break
